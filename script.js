512af9cde5ae5a2c38dc39b5639d839485a60ae1
valiantcreative33 <ruben@valiantcreative.net>
2022-06-29 00:05:24 -0400

    created player movement

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height +
@@ -36,11 +37,71 @@ class Player {
 }
 
 const player = new Player()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
 }
 
 animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left');
+            keys.left.pressed = true
+            break
+
+        case 83:
+            console.log('down');
+            break
+
+        case 68:
+            console.log('right');
+            keys.right.pressed = true
+            break
+
+        case 87:
+            console.log('up');
+            player.velocity.y -= 20
+            break
+    }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left');
+            keys.left.pressed = false
+            break
+
+        case 83:
+            console.log('down');
+            break
+
+        case 68:
+            console.log('right');
+            keys.right.pressed = false
+            break
+
+        case 87:
+            console.log('up');
+            player.velocity.y -= 15
+            break
+    }
+})
