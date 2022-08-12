b0406b62261080259753b5885cad24f48fe6ae24
valiantcreative33 <ruben@valiantcreative.net>
2022-08-11 22:43:28 -0400

    fine tuning – took away scroll to left of screen at game start

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -193,7 +193,11 @@ function animate() {
 
     if (keys.right.pressed && player.position.x < 500) {
         player.velocity.x = player.speed
-    } else if (keys.left.pressed && player.position.x > 400) {
+    } else if (
+        (keys.left.pressed && player.position.x > 400) ||
+        (keys.left.pressed && scrollOffset === 0 &&
+        player.position.x > 0)
+    ) {
         player.velocity.x = -player.speed
     } else {
         player.velocity.x = 0
@@ -208,7 +212,7 @@ function animate() {
             genericObject.position.x -= player.speed * .77
         })
         
-        } else if (keys.left.pressed) {
+        } else if (keys.left.pressed && scrollOffset > 0) {
             scrollOffset -= player.speed
             platforms.forEach((platform) => {
             platform.position.x += player.speed
